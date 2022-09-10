3818b8dd6be09be5bc6ac83563255d97d16e9a1b
Mato <tissuepaperarelimited@gmail.com>
2022-09-10 12:05:33 +0800

    Initial Commit

---
+++ VideoPlayerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoPlayerDemo
+//
+//  Created by kittawat phuangsombat on 2022/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoPlayerDemo/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by kittawat phuangsombat on 2022/9/10.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
